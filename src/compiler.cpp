8b495641a3a0bb859d0181f09c42bba92bc19c42
Dima Galkin <dima@dimlap.localdomain>
2023-08-25 03:13:11 -0700

    Major Refactor

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,16 +1,14 @@
 #include "compiler.hpp"
 
-#include <iomanip>
-
 bool SCompiler::set_output(std::string format) {
     if (format == "D8") {
-        output_type = HALF_DUAL_WORD;
+        output_type_ = HALF_DUAL_WORD;
         return true;
     } else if (format == "S8") {
-        output_type = HALF_SINGLE_WORD;
+        output_type_ = HALF_SINGLE_WORD;
         return true;
     } else if (format == "S16") {
-        output_type = FULL_SINGLE_WORD;
+        output_type_ = FULL_SINGLE_WORD;
         return true;
     }
     
@@ -18,14 +16,13 @@ bool SCompiler::set_output(std::string format) {
 }
 
 void SCompiler::split(std::string str, std::string const delim, std::vector<std::string>& out) {
+    const size_t delim_len = delim.length();
     size_t pos = 0;
-    std::string token;
 
     while ((pos = str.find(delim)) != std::string::npos) {
-    token = str.substr(0, pos);
-    out.push_back(token);
-    str.erase(0, pos + delim.length());
-}
+        out.push_back(str.substr(0, pos));
+        str.erase(0, pos + delim_len);
+    }
 
     if (str.size() > 0) {
         out.push_back(str);
@@ -48,7 +45,8 @@ Errors SCompiler::load_file(const std::string file_name) {
             if (!line.empty() && line[line.size() - 1] == '\r') {
                 line.erase(line.size() - 1);
             }
-            lines.push_back(line);
+
+            lines_.push_back(line);
         }
     }
 
@@ -58,7 +56,7 @@ Errors SCompiler::load_file(const std::string file_name) {
 }
 
 Errors SCompiler::locate_instruction(const std::string inst_name) {
-    for (const auto& [name, data] : instructions) {
+    for (const auto& [name, _] : instructions_) {
         if (name == inst_name) {
             return OK;
         }
@@ -68,7 +66,7 @@ Errors SCompiler::locate_instruction(const std::string inst_name) {
 }
 
 bool SCompiler::is_valid_reg(const std::string reg) {
-    for (const auto& [name, data] : registers) {
+    for (const auto& [name, _] : registers_) {
         if (name == reg) {
             return true;
         }
@@ -84,7 +82,9 @@ bool SCompiler::is_valid_imm(const std::string imm) {
     }
 
     for (const char& digit : imm.substr(2)) {
-        if (!isxdigit(digit)) { return false; }
+        if (!isxdigit(digit)) {
+            return false;
+        }
     }
 
     if (std::stoi(imm, 0, 16) > 0xFFFF){
@@ -97,14 +97,14 @@ bool SCompiler::is_valid_imm(const std::string imm) {
 }
 
 TKCptr SCompiler::make_token(Instruction inst, const std::string arg, const std::string inst_name, const int token_num) {
-    if (inst.child_types.at(token_num) == REG) {
+    if (inst.child_types_.at(token_num) == REG) {
         if (is_valid_reg(arg)) {
             return std::make_shared<Register>(REG, arg);
         } else {
             std::cout << "Invalid register: " << arg << ", for instruction " << inst_name << '\n';
             return nullptr;
         }
-    } else if (inst.child_types.at(token_num) == IMM) {
+    } else if (inst.child_types_.at(token_num) == IMM) {
         if (is_valid_imm(arg)) {
             return std::make_shared<Immediate>(IMM, arg);
         } else {
@@ -119,7 +119,7 @@ TKCptr SCompiler::make_token(Instruction inst, const std::string arg, const std:
             valid_imm = is_valid_imm(arg);
         }
 
-        if ((valid_imm || valid_reg) && inst.child_types.at(token_num) == REG_IMM) {
+        if ((valid_imm || valid_reg) && inst.child_types_.at(token_num) == REG_IMM) {
             if (valid_reg) {
                 return std::make_shared<Register>(REG, arg);
             } else {
@@ -133,10 +133,10 @@ TKCptr SCompiler::make_token(Instruction inst, const std::string arg, const std:
 }
 
 std::string SCompiler::htos(const std::string hex_value) {
-    return std::bitset< 16 >( std::stoi(hex_value, 0, 16)).to_string();
+    return std::bitset<16>(std::stoi(hex_value, 0, 16)).to_string();
 }
 
-InstPtr SCompiler::get_instruction_struct(const Instructions inst) {
+InstPtr SCompiler::get_instruction_struct(const Instructions& inst) {
     switch (inst) {
         case(MOV):
             return std::make_shared<MOV_Inst>();
@@ -158,13 +158,15 @@ InstPtr SCompiler::get_instruction_struct(const Instructions inst) {
             return std::make_shared<JNE_Inst>();
         case(JMP):
             return std::make_shared<JMP_Inst>();
+        case(EXEC):
+            return std::make_shared<EXEC_Inst>();
         default:
             return nullptr;
     };
 }
 
 Errors SCompiler::parse_file() {
-    for (const std::string line : lines) {
+    for (const std::string& line : lines_) {
         std::vector<std::string> line_tokens;
         std::vector<std::string> first_part;
 
@@ -186,23 +188,23 @@ Errors SCompiler::parse_file() {
                 return invalid_instruction;
             }
 
-            Instruction inst = instructions.at(inst_name);
+            Instruction inst = instructions_.at(inst_name);
 
-            if (inst.children != line_tokens.size() && !(inst.children == 0 && line_tokens.size() == 1)) {
+            if (inst.children_ != line_tokens.size() && !(inst.children_ == 0 && line_tokens.size() == 1)) {
                 std::cout << "Invalid number of arguments for instruction: " << inst_name << '\n';
                 return invalid_instruction;
             }
 
-            TKptr inst_token = std::make_shared<Token>(inst.type, std::vector<TKCptr>());
+            TKptr inst_token = std::make_shared<Token>(inst.type_, std::vector<TKCptr>());
 
-            if (inst.children == 0) {
-                tokens.push_back(inst_token);
+            if (inst.children_ == 0) {
+                tokens_.push_back(inst_token);
                 continue;
             }
 
             int token_num = 0;
 
-            for (std::string arg : line_tokens) {
+            for (std::string& arg : line_tokens) {
 
                 arg.erase(std::remove(arg.begin(), arg.end(), ' '), arg.end());
 
@@ -212,12 +214,12 @@ Errors SCompiler::parse_file() {
                     return invalid_instruction;
                 }
 
-                inst_token->children.push_back(token);
+                inst_token->children_.push_back(token);
 
                 ++token_num;
             }
 
-            tokens.push_back(inst_token);
+            tokens_.push_back(inst_token);
         }
     }
 
@@ -225,9 +227,9 @@ Errors SCompiler::parse_file() {
 }
 
 Errors SCompiler::parse_tree() {
-    for (const auto& tree_parent : tokens) {
+    for (const auto& tree_parent : tokens_) {
         int child_num = 0;
-        InstPtr inst = get_instruction_struct(tree_parent->type);
+        InstPtr inst = get_instruction_struct(tree_parent->type_);
 
         Types first;
         Types second;
@@ -235,22 +237,22 @@ Errors SCompiler::parse_tree() {
         std::string first_value;
         std::string second_value;
 
-        for (const auto& tree_child: tree_parent->children) {
-            if (tree_child->type == REG) {
+        for (const auto& tree_child: tree_parent->children_) {
+            if (tree_child->type_ == REG) {
                 if (child_num == 0) {
                     first = REG;
-                    first_value = tree_child->name;
+                    first_value = tree_child->name_;
                 } else {
                     second = REG;
-                    second_value = tree_child->name;
+                    second_value = tree_child->name_;
                 }
-            } else if (tree_child->type == IMM) {
+            } else if (tree_child->type_ == IMM) {
                 if (child_num == 0) {
                     first = IMM;
-                    first_value = tree_child->value;
+                    first_value = tree_child->value_;
                 } else {
                     second = IMM;
-                    second_value = tree_child->value;
+                    second_value = tree_child->value_;
                 }
             }
 
@@ -259,7 +261,7 @@ Errors SCompiler::parse_tree() {
 
         std::string binary_string = inst->get_code(first, second, first_value, second_value);
         
-        output += binary_string;
+        output_ += binary_string;
     }
 
     return OK;
@@ -273,16 +275,13 @@ Errors SCompiler::write_file() {
     std::stringstream low_data;
 
     std::vector<std::string> binary_strings;
-    split(output, "\n", binary_strings);
+    split(output_, "\n", binary_strings);
 
-    for(auto line : binary_strings) {
-        std::string high_byte_s = line.substr(0, 8);
-        std::string low_byte_s = line.substr(8, 8);
+    for(const auto& line : binary_strings) {
+        std::bitset<8> high_byte {line.substr(0, 8)};
+        std::bitset<8> low_byte {line.substr(8, 8)};
 
-        std::bitset<8> high_byte (high_byte_s);
-        std::bitset<8> low_byte (low_byte_s);
-
-        if (output_type == HALF_SINGLE_WORD) {
+        if (output_type_ == HALF_SINGLE_WORD) {
             high_data << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << high_byte.to_ulong();
             high_data << " ";
             low_data << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << low_byte.to_ulong();
@@ -290,7 +289,7 @@ Errors SCompiler::write_file() {
         } else {
             file_data << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << high_byte.to_ulong();
             
-            if (output_type == HALF_DUAL_WORD) {
+            if (output_type_ == HALF_DUAL_WORD) {
                 file_data << " ";
             }
 
@@ -299,7 +298,7 @@ Errors SCompiler::write_file() {
         }
     }
 
-    if (output_type == HALF_SINGLE_WORD) {
+    if (output_type_ == HALF_SINGLE_WORD) {
         std::ofstream high {"high.hex"};
         std::ofstream low {"low.hex"};
 
@@ -332,27 +331,19 @@ Errors SCompiler::write_file() {
 }
 
 int SCompiler::compile(const std::string filename) {
-    Errors read = load_file(filename);
-    
-    if (read != OK) {
+    if (load_file(filename) != OK) {
         return 1;
     }
 
-    Errors pf = parse_file();
-    
-    if (pf != OK) {
+    if (parse_file() != OK) {
         return 1;
     }
-    
-    Errors pt = parse_tree();
-    
-    if (pt != OK) {
-        return 1;
-    }
-    
-    Errors write = write_file();
 
-    if (write != OK) {
+    if (parse_tree() != OK) {
+        return 1;
+    }
+
+    if (write_file() != OK) {
         return 1;
     }
 
--- src/compiler.hpp
+++ src/compiler.hpp
@@ -3,44 +3,126 @@
 #include <vector>
 #include <string>
 #include <memory>
-#include <map>
+#include <unordered_map>
 #include <fstream>
 #include <sstream>
 #include <bitset>
+#include <iomanip>
 
-#include "types.hpp"
-#include "structs.hpp"
+enum Types {
+    REG,
+    IMM,
+    REG_IMM,
+    NONE
+};
+
+enum OutputTypes {
+    HALF_DUAL_WORD, // EEPROM which outputs data at address n and n+1, 2 8 bit outputs
+    HALF_SINGLE_WORD, // EEPROM which outputs data at address n so 2 have to be used as data width is 8 bits
+    FULL_SINGLE_WORD // EEPROM which outputs data at address n but data width is 16 bits
+};
+
+enum Errors {
+    OK,
+    file_not_found,
+    file_write_error,
+    invalid_instruction,
+    invalid_register,
+    invalid_immediate,
+};
+
+enum Instructions {
+    MOV,
+    WR,
+    RD,
+    EXEC,
+    ADD,
+    SUB,
+    JMP,
+    CMP,
+    JE,
+    JNE,
+    HLT
+};
+
+struct Token_Child {
+    public:
+        Types type_ = NONE;
+        std::string name_ = "";
+        std::string value_ = "";
+
+        Token_Child() = default;
+        virtual ~Token_Child() = default;
+};
+using TKCptr = std::shared_ptr<Token_Child>;
+
+struct Token {
+    Instructions type_;
+
+    std::vector<TKCptr> children_;
+
+    Token(Instructions type, std::vector<TKCptr> children) {
+        type_ = type;
+        children_ = children;
+    }
+
+    virtual ~Token() = default;
+};
+using TKptr = std::shared_ptr<Token>;
+
+struct Register : public Token_Child {
+    Register(Types type, std::string name) {
+        type_ = type;
+        name_ = name;
+    }
+};
+using REGptr = std::shared_ptr<Register>;
+
+struct Immediate : public Token_Child {
+    Immediate(Types type, std::string value) {
+        type_ = type;
+        value_ = value;
+    }
+};
+using IMMptr = std::shared_ptr<Immediate>;
+
+struct InstructionCode {
+    public:
+        virtual std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) = 0;
+        virtual ~InstructionCode() = default;
+};
+using InstPtr = std::shared_ptr<InstructionCode>;
 
 struct RegisterCode {
-    std::string name;
-    std::string from_bus;
-    std::string to_bus;
+    std::string name_;
+    std::string from_bus_;
+    std::string to_bus_;
 
     RegisterCode(std::string name, std::string from_bus, std::string to_bus) {
-        this->name = name;
-        this->from_bus = from_bus;
-        this->to_bus = to_bus;
+        name_ = name;
+        from_bus_ = from_bus;
+        to_bus_ = to_bus;
     }
 };
 
 struct Instruction {
-    std::string name;
+    std::string name_;
 
-    int children;
-    std::vector<Types> child_types;
-    Instructions type;
+    int children_;
+    std::vector<Types> child_types_;
+    Instructions type_;
 
     Instruction(std::string name, int children, std::vector<Types> child_types, Instructions type) {
-        this->name = name;
-        this->children = children;
-        this->child_types = child_types;
-        this->type = type;
+        name_ = name;
+        children_ = children;
+        child_types_ = child_types;
+        type_ = type;
     }
 
     virtual ~Instruction() = default;
 };
 
-const std::map<std::string, RegisterCode> register_codes = {
+const std::unordered_map<std::string, RegisterCode> register_codes = {
     {"a", RegisterCode("a", "00001", "00001")},
     {"b", RegisterCode("b", "00011", "00010")},
     {"c", RegisterCode("c", "01000", "00111")},
@@ -51,10 +133,11 @@ const std::map<std::string, RegisterCode> register_codes = {
 
 class SCompiler {
     private:
-        const std::map<std::string, Instruction> instructions = {
-            {"mov", Instruction("mov", 2, {REG, REG_IMM}, MOV)},
+        const std::unordered_map<std::string, Instruction> instructions_ = {
+            {"mov",  Instruction("mov", 2, {REG, REG_IMM}, MOV)},
             {"wr", Instruction("wr", 2, {REG_IMM, REG}, WR)},
             {"rd", Instruction("wr", 2, {REG_IMM, REG}, RD)},
+            {"exec", Instruction("exec", 1, {REG_IMM}, EXEC)},
             {"add", Instruction("add", 2, {REG_IMM, REG_IMM}, ADD)},
             {"sub", Instruction("sub", 2, {REG_IMM, REG_IMM}, SUB)},
             {"cmp", Instruction("cmp", 2, {REG_IMM, REG_IMM}, CMP)},
@@ -64,7 +147,7 @@ class SCompiler {
             {"hlt", Instruction("hlt", 0, {}, HLT)}
         };
 
-        const std::map<std::string, Register> registers = {
+        const std::unordered_map<std::string, Register> registers_ = {
             {"a", Register(REG, "a")},
             {"b", Register(REG, "b")},
             {"c", Register(REG, "c")},
@@ -73,40 +156,43 @@ class SCompiler {
             {"lgc", Register(REG, "lgc")},
         };
 
-        std::vector<std::string> lines;
-        std::vector<TKptr> tokens;
-        std::string output;
-        OutputTypes output_type = HALF_DUAL_WORD;
+        std::vector<std::string> lines_;
+        std::vector<TKptr> tokens_;
+        std::string output_;
+        OutputTypes output_type_;
 
-    public:
-
-        SCompiler() = default;
-
-        Errors load_file(std::string const file_name);
+        Errors load_file(const std::string file_name);
         Errors parse_file();
         Errors parse_tree();
         Errors write_file();
-        Errors locate_instruction(std::string const inst_name);
+        Errors locate_instruction(const std::string inst_name);
+    
+        TKCptr make_token(Instruction inst, const std::string reg, const std::string imm, const int token_num);
+        InstPtr get_instruction_struct(const Instructions& inst);
+        
+        void split (std::string str, const std::string delim, std::vector<std::string>& out);
 
-        bool set_output(std::string const format);
-        InstPtr get_instruction_struct(Instructions const inst);
-        void split (std::string str, std::string const delim, std::vector<std::string>& out);
-        int compile(std::string const file_name);
-        bool is_valid_reg(std::string const reg);
-        bool is_valid_imm(std::string const imm);
-        static std::string htos(std::string const hex_value);
-        TKCptr make_token(Instruction inst, std::string const reg, std::string const imm, int const token_num);
+        bool is_valid_reg(const std::string reg);
+        bool is_valid_imm(const std::string imm);
+    public:
+        // TODO: Set most methods to private;
+        SCompiler() {
+            output_type_ = HALF_DUAL_WORD;
+        };
+
+        static std::string htos(const std::string hex_value);
+        bool set_output(const std::string format);
+        int compile(const std::string file_name);
 
         ~SCompiler() = default;
 };
 
 struct MOV_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         if (first == REG && second == REG) {
-            return "0000" + register_codes.at(first_val).from_bus + register_codes.at(second_val).to_bus + "00\n";
+            return "0000" + register_codes.at(first_val).from_bus_ + register_codes.at(second_val).to_bus_ + "00\n";
         } else if (first == REG && second == IMM) {
-            return "0000" + register_codes.at(first_val).from_bus + "0000001\n" + SCompiler::htos(second_val) + "\n";
+            return "0000" + register_codes.at(first_val).from_bus_ + "0000001\n" + SCompiler::htos(second_val) + "\n";
         }
 
         return "error";
@@ -116,7 +202,6 @@ public:
 };
 
 struct HLT_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         return SCompiler::htos("0x7C") + "\n";
     }
@@ -125,19 +210,18 @@ public:
 };
 
 struct WR_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         // load address into mar (from imm or from reg)
         std::string mar_code = "";
         std::string wr_code = "";
 
         if (first == REG) {
-            mar_code = "000000111" + register_codes.at(first_val).to_bus + "00\n";
+            mar_code = "000000111" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             mar_code = "0000001110000001\n" + SCompiler::htos(first_val) + "\n";
         }
 
-        wr_code = "100000010" + register_codes.at(second_val).to_bus + "11\n";
+        wr_code = "100000010" + register_codes.at(second_val).to_bus_ + "11\n";
         
         return mar_code + wr_code;
     }
@@ -146,40 +230,58 @@ public:
 };
 
 struct RD_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         // load address into mar (from imm or from reg)
         std::string mar_code = "";
-        std::string wr_code = "";
+        std::string rd_code = "";
 
         if (first == REG) {
-            mar_code = "000000111" + register_codes.at(first_val).to_bus + "00\n";
+            mar_code = "000000111" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             mar_code = "0000001110000001\n" + SCompiler::htos(first_val) + "\n";
         }
 
-        wr_code = "1000" + register_codes.at(second_val).to_bus + "00000" + "01\n";
+        rd_code = "0000" + register_codes.at(second_val).from_bus_ + "01001" + "00\n";
         
-        return mar_code + wr_code;
+        return mar_code + rd_code;
     }
 
     virtual ~RD_Inst() = default;
 };
 
+struct EXEC_Inst : public InstructionCode {
+    std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
+        // load address into mar (from imm or from reg)
+        std::string mar_code = "";
+        std::string rd_code = "";
+
+        if (first == REG) {
+            mar_code = "000000111" + register_codes.at(first_val).to_bus_ + "00\n";
+        } else if (first == IMM) {
+            mar_code = "0000001110000001\n" + SCompiler::htos(first_val) + "\n";
+        }
+
+        rd_code = "0000010010100100\n";
+        
+        return mar_code + rd_code;
+    }
+
+    virtual ~EXEC_Inst() = default;
+};
+
 struct ADD_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string first_int = "";
         std::string second_int = "";
 
         if (first == REG) {
-            first_int = "000000100" + register_codes.at(first_val).to_bus +  "00\n";
+            first_int = "000000100" + register_codes.at(first_val).to_bus_ +  "00\n";
         } else if (first == IMM) {
             first_int = "0000001000000001\n" + SCompiler::htos(first_val) + "\n";
         }
 
         if (second == REG) {
-            second_int = "000100000" + register_codes.at(second_val).to_bus + "00\n";
+            second_int = "000100000" + register_codes.at(second_val).to_bus_ + "00\n";
         } else if (second == IMM) {
             second_int = "0001000000000001\n" + SCompiler::htos(second_val) + "\n";
         }
@@ -191,19 +293,18 @@ public:
 };
 
 struct SUB_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string first_int = "";
         std::string second_int = "";
 
         if (first == REG) {
-            first_int = "000000100" + register_codes.at(second_val).to_bus +  "00\n";
+            first_int = "000000100" + register_codes.at(second_val).to_bus_ +  "00\n";
         } else if (first == IMM) {
             first_int = "0000001000000001\n" + SCompiler::htos(second_val) + "\n";
         }
 
         if (second == REG) {
-            second_int = "001000000" + register_codes.at(first_val).to_bus + "00";
+            second_int = "001000000" + register_codes.at(first_val).to_bus_ + "00";
         } else if (second == IMM) {
             second_int = "0010000000000001\n" + SCompiler::htos(first_val) + "\n";
         }
@@ -215,12 +316,11 @@ public:
 };
 
 struct JMP_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string code = "";
 
         if (first == REG) {
-            code = "000000110" + register_codes.at(first_val).to_bus + "00\n";
+            code = "000000110" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             code = "0000001100000001\n" + SCompiler::htos(first_val) + "\n";
         }
@@ -232,20 +332,19 @@ public:
 };
 
 struct COMP_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string first_int = "";
         std::string second_int = "";
 
         if (second == REG) {
             std::cout << "first is reg" << std::endl;
-            first_int = "000000100" + register_codes.at(second_val).to_bus +  "00\n";
+            first_int = "000000100" + register_codes.at(second_val).to_bus_ +  "00\n";
         } else if (second == IMM) {
             first_int = "0000001000000001\n" + SCompiler::htos(second_val) + "\n";
         }
 
         if (first == REG) {
-            second_int = "001100000" + register_codes.at(first_val).to_bus + "00\n";
+            second_int = "001100000" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             second_int = "0011000000000001\n" + SCompiler::htos(first_val) + "\n";
         }
@@ -257,12 +356,11 @@ public:
 };
 
 struct JE_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string code = "";
 
         if (first == REG) {
-            code = "000011111" + register_codes.at(first_val).to_bus + "00\n";
+            code = "000011111" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             code = "0000111110000001\n" + SCompiler::htos(first_val) + "\n";
         }
@@ -274,12 +372,11 @@ public:
 };
 
 struct JNE_Inst : public InstructionCode {
-public:
     std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
         std::string code = "";
 
         if (first == REG) {
-            code = "000011110" + register_codes.at(first_val).to_bus + "00\n";
+            code = "000011110" + register_codes.at(first_val).to_bus_ + "00\n";
         } else if (first == IMM) {
             code = "0000111100000001\n" + SCompiler::htos(first_val) + "\n";
         }
@@ -288,21 +385,4 @@ public:
     }
 
     virtual ~JNE_Inst() = default;
-};
-
-struct AND_Inst : public InstructionCode {
-public:
-    std::string get_code(const Types first, const Types second, const std::string first_val, const std::string second_val) override {
-        std::string code = "";
-
-        if (first == REG) {
-            code = "000011110" + register_codes.at(first_val).to_bus + "00\n";
-        } else if (first == IMM) {
-            code = "0000111100000001\n" + SCompiler::htos(first_val) + "\n";
-        }
-
-        return code;
-    }
-
-    virtual ~AND_Inst() = default;
 };
